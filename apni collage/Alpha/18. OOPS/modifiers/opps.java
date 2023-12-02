7c841ca3ec2f9d9a799ebd279269b9b6d46b03db
its-tusharAg <officialtjtushar@gmail.com>
2023-12-02 18:35:12 +0530

    Just ad all my files

---
+++ apni collage/Alpha/18. OOPS/modifiers/opps.java
@@ -0,0 +1,109 @@
+package modifiers;
+import java.util.*;
+
+
+public class opps {
+    public static void main(String[] args) {
+        // Pennfo
+        Pen p1 = new Pen();
+        p1.setColor("yello");
+        
+        System.out.println(p1.color);
+        System.out.println(p1.getColor());
+        p1.setSize(2);
+        System.out.println(p1.getSize());
+
+        //Student function
+        Student s1 = new Student();
+        s1.setName("tushar");
+        s1.setAge(20);
+        s1.setCgpa((float)54.34324);
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.cgpa);
+
+        //My Information
+        MySelf ms1 = new MySelf();
+        ms1.setName("Tushar agarwal");
+        ms1.setAge(20);
+        ms1.setPName("thombson");
+        System.out.println(ms1.name);
+        System.out.println(ms1.petName);
+        System.out.println(ms1.age);
+
+        //BankINfo
+        BankAccount ba = new BankAccount();
+        ba.name = "tjtushar";
+        // ba.passward = "1234";
+        ba.setPass("123");
+        System.out.println(ba.name);
+        System.out.println(ba.getPassward());
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+    int size;
+
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+    String getColor(){
+        return this.color;
+    }
+    void setSize(int newSize){
+        size = newSize;
+    }
+    int getSize(){
+        return this.size;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+class Student{
+    String name;
+    int age;
+    Float cgpa;
+
+    void setName(String newName){
+        name = newName;
+    }
+    void setAge(int newAge){
+        age = newAge;
+    }
+    void setCgpa(Float newCgpa){
+        cgpa = newCgpa;
+    }
+}
+
+class MySelf{
+    String name;
+    int age;
+    String petName;
+
+    void setName(String newName){
+        name = newName;
+    }
+    void setAge(int newAge){
+        age = newAge;
+    }
+    void setPName(String newPName){
+        petName = newPName;
+    }
+}
+class BankAccount{
+    public String name;
+    private String passward;
+
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setPass(String newPass){
+        this.passward = newPass;
+    }
+    String getPassward(){
+        return this.passward;
+    }
+}
