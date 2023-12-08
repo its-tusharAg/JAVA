ad9595ff0015b5e7987d32e892c0e3d301b9a0b9
its-tusharAg <officialtjtushar@gmail.com>
2023-12-08 10:42:15 +0530

    adding some oops code

--- apni collage/Alpha/18. OOPS/questions/lecture question/question1/Question.java
+++ apni collage/Alpha/18. OOPS/questions/lecture question/question1/Question.java
@@ -2,11 +2,16 @@ public class Question {
 
     public static void main(String[] args) {
         Student s = new Student();
-        s.name = "Tushar";
-        System.out.println(s.name);
+        s.setName("Tushar");
+        System.out.println(Student.name);
+
     }
 }
 class Student{
-    String name;
+    static String name;
     int marks;
+    void setName(String name){
+        this.name = name;
+    }
 }
+
